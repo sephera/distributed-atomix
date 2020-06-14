cae79afe1d5c459036c8f2fc31f20adc35716eb4
Hai Nguyen <haind.vng@gmail.com>
2020-06-14 22:34:11 +0700

    init project

---
+++ src/main/java/com/example/distributedatomix/DistributedAtomixApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributedatomix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedAtomixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedAtomixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/distributedatomix/DistributedAtomixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributedatomix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedAtomixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
